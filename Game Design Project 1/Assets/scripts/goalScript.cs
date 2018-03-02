7e68f782395eef4ca99f8edc54883907b04d6eb2
simonlhopkins <simonlhopkins@gmail.com>
2018-03-02 18:12:02 -0500

    Game Design Project 1

---
+++ Game Design Project 1/Assets/scripts/goalScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class goalScript : pressurePlate {
+
+	public Text descriptionText; 
+	public string newLevelName;
+	public enum textNameOptions
+	{
+		mainText1,
+		mainText2,
+		mainText3
+	};
+	public textNameOptions writingOption;
+	public string requiredItemToShow;
+	public string addedInventoryItem;
+	public string removedFromInventory;
+	GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.FindGameObjectWithTag("gameManager");
+		if(requiredItemToShow!=""){
+			if (gameManager.GetComponent<gameManagerScript> ().playerInventory.Contains (requiredItemToShow)) {
+				gameObject.SetActive (true);
+			} else {
+				gameObject.SetActive (false);
+			}
+		}
+		descriptionText.gameObject.SetActive (false);
+		compressionDownVector = transform.position- (transform.up/30f);
+		compressionUpVector = transform.position+ (transform.up/20f);
+	}
+	void OnDisable(){
+		if(descriptionText!=null){
+			if (descriptionText.gameObject.activeInHierarchy) {
+				descriptionText.gameObject.SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pushDown ();
+		if (touchingPlayer) {
+			descriptionText.gameObject.SetActive (true);
+
+		} else {
+			descriptionText.gameObject.SetActive (false);
+
+		}
+		if (Input.GetKeyDown (KeyCode.E) && touchingPlayer) {
+			if (addedInventoryItem != "") {
+				print("add");
+				gameManager.GetComponent<gameManagerScript> ().addToInventory (addedInventoryItem);
+
+			}
+			if (removedFromInventory != "") {
+				gameManager.GetComponent<gameManagerScript> ().removeFromInventory (removedFromInventory);
+			}
+			SceneManager.LoadScene (newLevelName);
+
+			gameManager.GetComponent<gameManagerScript> ().mainTextOption = writingOption.ToString();
+			if (newLevelName != SceneManager.GetActiveScene ().name) {
+				return;
+			}
+
+		}
+
+	}
+}
---
+++ Game Design Project 1/Assets/scripts/transporterScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transporterScript : pressurePlate {
+
+	public GameObject destination;
+
+	void Update(){
+		pushDown ();
+		if (transform.position == compressionDownVector) {
+			GameObject.Find("player").transform.position = destination.transform.position + (destination.transform.up/2f);
+		}
+
+	}
+}
